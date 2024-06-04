4f6023802e8fde0a6d3fa65414f35a5fe0e0309a
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-06-04 14:46:34 +0200

    rinominato il progetto

--- src/main/java/marinalucentini/Application.java
+++ src/main/java/marinalucentini/Application.java
@@ -4,7 +4,7 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class Application {
-    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("w4d2");
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestioneeventi");
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
