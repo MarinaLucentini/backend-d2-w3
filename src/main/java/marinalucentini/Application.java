d0e50abeb2193d491a33320e07b99d6f042a40d5
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-06-04 14:42:13 +0200

    collegamento con il db

--- src/main/java/marinalucentini/Application.java
+++ src/main/java/marinalucentini/Application.java
@@ -1,8 +1,13 @@
 package marinalucentini;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("w4d2");
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
 }
